b3edde2653d85d88de4bfe642f007e91fe3061d2
alen-marutyan <alen.marutyan@inbox.ru>
2022-11-11 18:35:17 +0400

    add origin

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const http = require("http");
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 5000
-const server = http.createServer(app)
+const server = http.createServer(app);
+const corsOption = {
+    origin: ["https://developer.riotgames.com"],
+};
+
+app.use(cors(corsOption))
 
 app.use(logger('dev'));
 app.use(express.json());
